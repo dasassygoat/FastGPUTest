c74e0cf415c73bd95f07d337417c5ffdf72fece2
Journeyman32 <bryan@coderbox.co>
2023-03-06 23:10:44 -0600

    correct gitignore file

---
+++ FastGPUTest/kernel.cu
@@ -0,0 +1,27 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+int main()
+{
+
+    int a[] = { 1,2,3 };
+    int b[] = { 4,5,6 };
+    int c[sizeof(a) / sizeof(int)] = { 0 };
+
+    int* cudaA = 0;
+    int* cudaB = 0;
+    int* cudaC = 0;
+
+    for(int i = 0; sizeof(c)/sizeof(int) > i; i++)
+    {
+        c[i] = a[i] + b[i];
+
+
+    }
+
+    return;
+}
+
